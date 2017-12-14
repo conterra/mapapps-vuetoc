5a375c277ef20af683003c3ae505f30d0b615a5d
Matthias Stein <m.stein@conterra.de>
2017-12-14 12:13:30 +0100

    Update to map.apps 4.2.2

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ const run_sequence = require('run-sequence');
 const mapapps = require('ct-mapapps-gulp-js');
 
 mapapps.registerTasks({
-    themes: ["sample_theme"],
+    themes: [],
     hasBaseThemes: true,
     forceTranspile: true
 });
