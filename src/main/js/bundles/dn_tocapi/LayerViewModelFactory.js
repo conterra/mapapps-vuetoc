3d639620d9ed3824a9b07e0f8d225888eef6a1f7
reun <r.unrau@conterra.de>
2019-04-12 18:58:59 +0200

    remove obsolete import

--- src/main/js/bundles/dn_tocapi/LayerViewModelFactory.js
+++ src/main/js/bundles/dn_tocapi/LayerViewModelFactory.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import Layer from "esri/layers/Layer";
 import LayerViewModel from "./LayerViewModel";
 import Binding from "apprt-binding/Binding"
 
